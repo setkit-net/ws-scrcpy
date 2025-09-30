79fea23e644a9f38148c99d8f458c64338978894
setkit <setkitnet@gmail.com>
2025-09-30 23:53:10 +0700

    Update ControlCenter.ts

--- src/server/goog-device/services/ControlCenter.ts
+++ src/server/goog-device/services/ControlCenter.ts
@@ -142,7 +142,7 @@ export class ControlCenter extends BaseControlCenter<GoogDeviceDescriptor> imple
     }
 
     public getName(): string {
-        return `aDevice Tracker [${os.hostname()}]`;
+        return `[${os.hostname()}]`;
     }
 
     public start(): Promise<void> {
